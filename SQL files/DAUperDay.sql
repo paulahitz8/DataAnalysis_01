03dd1dbb0fb831ddde486e324457c4a5e272ee7e
Needlesslord <51851736+Needlesslord@users.noreply.github.com>
2022-11-03 23:02:47 +0100

    KPIs finished

---
+++ SQL files/DAUperDay.sql
@@ -0,0 +1,1 @@
+SELECT * FROM paulahm.DAUperDay;
--- SQL files/LengthAverageSession.sql
+++ SQL files/LengthAverageSession.sql
@@ -1,8 +1,8 @@
-select date(mydau.date) as "Day",
-ifnull(sum(minute(timediff(Sessions.SessionEnd, Sessions.SessionStart))) / count(mydau.active_users), 0) 
-as "average_length"
+select date(DAUperDay.Date) as "Day",
+ifnull(sum(minute(timediff(Sessions.SessionEnd, Sessions.SessionStart))) / count(DAUperDay.Dau), 0) 
+as "Average Length"
 from Sessions
-right join mydau
-on date(Sessions.SessionStart) = date(mydau.date)
+right join DAUperDay
+on date(Sessions.SessionStart) = date(DAUperDay.Date)
 group by 1
 order by 1
